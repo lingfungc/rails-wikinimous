ba8dc59ca0b2ad2fea7868ae4cee251adfd41320
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-15 11:09:51 +0000

    completed articles controller with 7 functions for CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,23 +9,35 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 
   def edit
   end
 
   def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path, status: :see_other
   end
 
   private
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
