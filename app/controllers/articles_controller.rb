f24907d59527cd23accd57c2db1ed34975442493
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-15 08:59:04 +0000

    updated link_to in articles#index and completed articles#show and show.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
+  end
+
+  def new
+  end
+
+  def create
   end
 
   def edit
@@ -13,5 +21,11 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
