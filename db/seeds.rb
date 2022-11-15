4a487f254ccc1e2585e9bda1e239f7a0a2ebace0
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-15 08:45:02 +0000

    added faker gem and generated 10 fake articles in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  title = Faker::Books::Dune.character
+  content = Faker::Books::Dune.quote
+  new_article = Article.new(title: title, content: content)
+  new_article.save
+end
