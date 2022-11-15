eab3f36c6f9cd7d7f26a141ac952644eb22427ba
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-15 08:47:16 +0000

    generated articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/edit'
+  get 'articles/update'
+  get 'articles/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
