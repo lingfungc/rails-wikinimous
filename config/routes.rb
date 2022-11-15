8cd51254a9947ca3e00b643f50ef446bfe5c7ef6
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-15 08:48:27 +0000

    added articles resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/edit'
-  get 'articles/update'
-  get 'articles/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles
 end
